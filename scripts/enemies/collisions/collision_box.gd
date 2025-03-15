60afe14a18ca213182cb892c5b45e54b5f26e915
Gabriel Luís da Silva <officialmercurymusicdj@gmail.com>
2025-03-15 19:04:44 -0300

    Dano nos inimigos

--- scripts/enemies/collisions/collision_box.gd
+++ scripts/enemies/collisions/collision_box.gd
@@ -1,7 +1,11 @@
 extends Area2D
+class_name EnemyCollisionBox
 
 @export var enemy: Enemy
 @export var damage: float
 
 func _on_body_entered(body: Node2D) -> void:
 	enemy.causePlayerDamage(damage)
+	
+func onPlayerCauseDamage(damage: float) -> void:
+	enemy.applyDamage(damage);
--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -3,12 +3,24 @@ class_name Enemy
 
 var targetDirection = Vector2(0,0);
 const SPEED = 10;
+@export var health = 100;
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	position.x += targetDirection.x * SPEED * delta
 	position.y += targetDirection.y * SPEED * delta;
+	
+func applyDamage(damage: float) -> void:
+	health -= damage;
+	verifyDeath()
+	
+func die() -> void:
+	queue_free()	
+
+func verifyDeath() -> void:
+	if(health <= 0):
+		die()
 
 func causePlayerDamage(damage: float) -> void:
 	GameManager.applyDamage(damage)
