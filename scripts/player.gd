5133dacb58a2ce4a71bd5ef9213d9ed1f7e12078
ALLVesgo <dheeiner@gmail.com>
2025-03-15 23:00:39 -0300

    ataque basico adicionado

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,21 +3,18 @@ extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var coyote_timer: Timer = $CoyoteTimer
-@onready var fire_cooldown: Timer = $FireCooldown
-@onready var marker_2d: Marker2D = $Marker2D
-
-const FIRE = preload("res://scenes/fire.tscn")
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 const VELOCIDADE_LIM = 300.0  
 const DANO_MULTI = 0.5   
 var can_jump = false
-var can_shoot = true
- 
+var is_attacking : bool = false
 
 var velocidade_queda = 0.0
 var  posicao_inicial = Vector2.ZERO  # Posição inicial
+
+
 
 func _ready() -> void:
 	posicao_inicial = position
@@ -26,7 +23,7 @@ func _physics_process(delta: float) -> void:
 	# Adicionar gravidade.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		velocidade_queda = velocity.y
+		velocidade_queda = velocity.y 
 	else:
 		coyote_timer.start(0.09)
 		can_jump = true
@@ -40,54 +37,55 @@ func _physics_process(delta: float) -> void:
 	# Lidar com o pulo
 	if Input.is_action_just_pressed("jump") and can_jump == true:
 		velocity.y = JUMP_VELOCITY
-		can_jump = false
 
 	# Obter a direção de movimento: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
-	
-	# Virar o Sprite dependendo da direção
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-			
-	# Reproduzir a animação
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
+	if !is_attacking:
+
+		# Reproduzir a animação
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-	
+			animated_sprite.play("jump")
+		
 	# Aplicar o movimento
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite.scale.x = direction 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("fire") and can_shoot == true:
-		shoot()
-		fire_cooldown.start(0.1)
-		
-# Controlar o tempo do "pulo do coyote"
-func _on_coyote_timer_timeout() -> void:
-	can_jump = false
-
-# Função para o personagem atirar
-func shoot():
-	can_shoot = false
-	var bullet = FIRE.instantiate()
-	owner.add_child(bullet)
-	bullet.position = marker_2d.global_position
-	
-	if animated_sprite.flip_h == false:
-		bullet.scale = Vector2(0.25, 0.25)
+	# Sistema de Ataque
+	if !is_attacking:
+		$AnimatedSprite2D/Hitbox/HitboxAttackMelee.disabled = true
 	else:
-		bullet.scale = Vector2(-0.25, 0.25)
+		$AnimatedSprite2D/Hitbox/HitboxAttackMelee.disabled = false
+	if Input.is_action_just_pressed("attack") and !is_attacking:
+		is_attacking = true
+		animated_sprite.play("attack")
 
-# Controlar o tempo de cooldown do tiro
-func _on_fire_cooldown_timeout() -> void:
-	can_shoot = true
+func _on_hitbox_body_shape_entered( body: Node2D) -> void:
+	print("Colisão detectada com:", body.name)
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == "attack":
+		is_attacking = false
+
+# Função para a morte do personagem
+#func die():
+#	print("Personagem morreu!")
+#	respawn()  
+# Função de respawn
+#func respawn():
+#	print("Personagem respawnou")
+
+
+func _on_hitbox_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if area.is_in_group("enemy"):
+		print("Colisão detectada com:", area.name)
+		area.queue_free()
