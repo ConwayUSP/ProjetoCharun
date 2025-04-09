11fb40e7923d4e78cef2c1ba80f2b47c7978a78e
eduardo <11eduardoalmeida@gmail.com>
2025-04-09 19:56:25 -0300

    Reajustes do AudioManager

--- scripts/breakable_tiles.gd
+++ scripts/breakable_tiles.gd
@@ -3,5 +3,3 @@ extends Area2D
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("playerAttack") : 
 		queue_free()
-		if AudioManager.blocoDestruido != null:
-			AudioManager.blocoDestruido.play()
