d686c715e8e748792a2f6b5c8c466c499a97a461
eduardo <11eduardoalmeida@gmail.com>
2025-04-11 12:02:28 -0300

    Fazendo o som do powerUp ativar por meio do AudioManager

--- scripts/roll.gd
+++ scripts/roll.gd
@@ -2,11 +2,10 @@ extends Area2D
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var powerUp: AudioStreamPlayer2D = $powerUp_som
 
 func _on_body_entered(_body: Node2D) -> void:
 	if animation_player.current_animation != "pickup":
-		if powerUp != null:
-			powerUp.play()
+		if AudioManager.powerUp != null:
+			AudioManager.powerUp.play()
 		GameManager.add_imunity()
 		animation_player.play("pickup")
