f174c5126c2ab9c7f1ef62fcd60187714e13746d
eduardo <11eduardoalmeida@gmail.com>
2025-05-17 12:46:15 -0300

    Reajustando o timer e criando função de remover o power up

--- scripts/roll.gd
+++ scripts/roll.gd
@@ -9,3 +9,5 @@ func _on_body_entered(_body: Node2D) -> void:
 			AudioManager.powerUp.play()
 		GameManager.add_imunity()
 		animation_player.play("pickup")
+		await get_tree().create_timer(10.0).timeout
+		GameManager.remove_imunity()
