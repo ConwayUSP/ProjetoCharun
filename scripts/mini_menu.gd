5d7e6ed19cbdc4751d5eecb0d3cf3f3b86fabb7e
Renan9590 <biruelrenan@gmail.com>
2025-05-18 10:48:17 -0300

    Proto-Menu
    Protótipo de menu de pause

--- scripts/mini_menu.gd
+++ scripts/mini_menu.gd
@@ -1,8 +1,26 @@
 extends Node
 
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+	
+	
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
 
 
-
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	
+	
+func test_esc():
+	if Input.is_action_just_pressed("menu") and get_tree().paused == false:
+		pause()
+		
+	elif Input.is_action_just_pressed("menu") and get_tree().paused == true:
+		resume()
+		
 
 func _on_reset_button_pressed() -> void:
 	GameManager.life = 100
@@ -11,9 +29,21 @@ func _on_reset_button_pressed() -> void:
 	GameManager.player.global_position.y = 1.0
 	GameManager.salvar()
 	get_tree().reload_current_scene()
-
-
-
+	
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_resume_button_pressed() -> void:
+	resume()
+	
+
+
+
+#func _on_last_checkpoint_button_pressed() -> void:
+	
+
+
+func _process(delta: float) -> void:
+	test_esc()
